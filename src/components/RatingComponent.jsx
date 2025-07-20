e08c1f3920aad72ed0cb2bd8912e34e48c7d701a
Ram Delatina <r2am9d@gmail.com>
2025-07-21 06:54:59 +0800

    chore: added stars

--- src/components/RatingComponent.jsx
+++ src/components/RatingComponent.jsx
@@ -1,8 +1,17 @@
 const RatingComponent = () => {
+  const arr = Array.from({ length: 5 }, (_, i) => i);
+
   return (
     <>
       <div className="rating-container">
         <h2>Rate your experience</h2>
+        <div className="stars">
+          {arr.map((e) => (
+            <span key={e} className="star">
+              {"\u2605"}
+            </span>
+          ))}
+        </div>
       </div>
     </>
   );
