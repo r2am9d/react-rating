06b793293b3239bcaff6e837d7eea7ca81997759
Ram Delatina <r2am9d@gmail.com>
2025-07-21 07:29:21 +0800

    chore: added initial event handlers

--- src/components/RatingComponent.jsx
+++ src/components/RatingComponent.jsx
@@ -1,13 +1,23 @@
 const RatingComponent = () => {
   const arr = Array.from({ length: 5 }, (_, i) => i);
+  const handler = (index) => (e) => console.log(`${index}`);
+  const hovered = (direction, index) => (e) =>
+    console.log(`${direction} | ${index}`);
 
   return (
     <>
       <div className="rating-container">
         <h2>Rate your experience</h2>
         <div className="stars">
-          {arr.map((e) => (
-            <span key={e} className="star">
+          {arr.map((e, i) => (
+            <span
+              onClick={handler(i)}
+              onMouseEnter={hovered("Enter", i)}
+              onMouseLeave={hovered("Leave", i)}
+              key={e}
+              className="star"
+              style={{ border: "1px solid red" }}
+            >
               {"\u2605"}
             </span>
           ))}
