42f28b92950fb247cd141fc889c231f25597c5c2
Ram Delatina <r2am9d@gmail.com>
2025-07-20 16:41:16 +0800

    chore: added rating component

---
+++ src/components/RatingComponent.jsx
@@ -0,0 +1,11 @@
+const RatingComponent = () => {
+  return (
+    <>
+      <div className="rating-container">
+        <h2>Rate your experience</h2>
+      </div>
+    </>
+  );
+};
+
+export default RatingComponent;
