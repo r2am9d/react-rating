1a0099a0a9bf1e57d29716dbabc579f212bd0a10
Ram Delatina <r2am9d@gmail.com>
2025-07-21 09:19:54 +0800

    chore: added proper styling

--- src/components/RatingComponent.jsx
+++ src/components/RatingComponent.jsx
@@ -1,29 +1,36 @@
+import { useState } from "react";
+
 const RatingComponent = () => {
-  const arr = Array.from({ length: 5 }, (_, i) => i);
-  const handler = (index) => (e) => console.log(`${index}`);
-  const hovered = (direction, index) => (e) =>
-    console.log(`${direction} | ${index}`);
+  const [rating, setRating] = useState(null);
+  const [hover, setHover] = useState(null);
+
+  const stars = Array.from({ length: 5 }, (_, i) => i);
+  const messages = ["Terrible", "Poor", "Fair", "Good", "Excellent"];
+
+  const onClick = (index) => (e) => setRating(index);
+  const onHover = (index) => (e) => setHover(index);
 
   return (
-    <>
-      <div className="rating-container">
-        <h2>Rate your experience</h2>
-        <div className="stars">
-          {arr.map((e, i) => (
+    <div className="rating-container">
+      <h2>Rate your experience</h2>
+      <div className="stars">
+        {stars.map((e, i) => {
+          const idx = i + 1;
+          return (
             <span
-              onClick={handler(i)}
-              onMouseEnter={hovered("Enter", i)}
-              onMouseLeave={hovered("Leave", i)}
+              onClick={onClick(idx)}
+              onMouseEnter={onHover(idx)}
+              onMouseLeave={onHover(null)}
               key={e}
-              className="star"
-              style={{ border: "1px solid red" }}
+              className={`star ${idx <= (hover || rating) ? "active" : ""}`}
             >
               {"\u2605"}
             </span>
-          ))}
-        </div>
+          );
+        })}
       </div>
-    </>
+      {rating && <div className="feedback">{messages[rating - 1]}</div>}
+    </div>
   );
 };
 
